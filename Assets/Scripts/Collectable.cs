34e03ec4838e4986391b251db54f4689f26d1f56
Jesper Seipenbusch <jesper.s.js@gmail.com>
2020-09-24 14:34:09 +0200

    Added CollectionManager & Collectable scripts
    NOTE: Manager objects position should be (0,0,0)
    NOTE: Main Camera position should be (0, 0, x)

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider _Coll)
+    {
+        CollectionManager.AddScore();
+        Destroy(this.gameObject);
+    }
+
+
+    //debug method (for mouse)
+    private void OnMouseOver()
+    {
+        CollectionManager.AddScore();
+        CollectionManager.RemoveCollectableCount();
+        Destroy(this.gameObject);
+    }
+
+
+    
+    private void Update()
+    {
+        transform.Rotate(0, 0, 50f * Time.deltaTime);
+    }
+}
