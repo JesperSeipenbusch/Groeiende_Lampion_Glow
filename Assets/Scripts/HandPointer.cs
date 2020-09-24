0088a42353a39f33789520da0f0cdb05b6a8d83b
Mazaki <gamerad94@gmail.com>
2020-09-24 13:59:33 +0200

    Add kinect motion tracking

---
+++ Assets/Scripts/HandPointer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPointer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Light"))
+        {
+            Debug.Log("Collected light");
+            StartCoroutine(Lights(col.gameObject));
+        }
+    }
+
+    private IEnumerator Lights(GameObject ob)
+    {
+        float i = Random.Range(2.0f, 4.5f);
+        ob.SetActive(false);
+        yield return new WaitForSeconds(i);
+        ob.SetActive(true);
+    }
+}
