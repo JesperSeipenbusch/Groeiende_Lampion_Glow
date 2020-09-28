c6e70510c000a7f42864696ebab68fdfda42df00
Jesper Seipenbusch <jesper.s.js@gmail.com>
2020-09-28 16:11:39 +0200

    Collectables now move to centre point when collected

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+
+    float t;
+    Vector3 startPosition;
+    Vector3 target;
+    float timeToReachTarget;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        target = new Vector3(0,0,0);
+    }
     private void OnTriggerEnter(Collider _Coll)
     {
-        CollectionManager.AddScore();
-        Destroy(this.gameObject);
+        CollectionManager.RemoveCollectableCount();
+        StartCoroutine(MoveToPosition(this.gameObject.transform, target, 1f));
     }
 
 
     //debug method (for mouse)
-    private void OnMouseOver()
+    private void OnMouseEnter()
     {
-        CollectionManager.AddScore();
         CollectionManager.RemoveCollectableCount();
-        Destroy(this.gameObject);
+        StartCoroutine(MoveToPosition(this.gameObject.transform, target, 1f));
+
     }
 
 
@@ -24,5 +35,27 @@ public class Collectable : MonoBehaviour
     private void Update()
     {
         transform.Rotate(0, 0, 50f * Time.deltaTime);
+        if(transform.position == target)
+        {
+            DestroyAndAdd();
+        }
+    }
+
+
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+   {
+        var currentPos = transform.position;
+        var t = 0f;
+        while(t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+    }
+    void DestroyAndAdd()
+    {
+        Destroy(this.gameObject);
+        CollectionManager.AddScore();
     }
 }
