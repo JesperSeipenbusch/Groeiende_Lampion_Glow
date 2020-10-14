f70d5417aab2529e765360832dbf02e0b8c88639
Mazaki <gamerad94@gmail.com>
2020-10-14 20:43:05 +0200

    Build environment

---
+++ Assets/Scripts/EmmisionGITest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmmisionGITest : MonoBehaviour
+{
+    Color color;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        color = rend.material.GetColor("_EmissionColor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = Mathf.Sin(Time.realtimeSinceStartup * 3.0f) * 0.5f + 0.5f;
+        Debug.Log(value);
+        DynamicGI.SetEmissive(rend, color * value);
+    }
+}
